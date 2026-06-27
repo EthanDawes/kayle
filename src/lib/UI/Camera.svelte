06ccde4c7fc46c52459c4858835c359f827aa491
Ethan Dawes <ethand.python@gmail.com>
2026-06-27 15:26:25 -0400

    Use portrait ratio

--- src/lib/UI/Camera.svelte
+++ src/lib/UI/Camera.svelte
@@ -32,7 +32,12 @@
     try {
       error = ""
       stream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment", width: { ideal: 1920 }, height: { ideal: 1080 } },
+        video: {
+          facingMode: "environment",
+          width: { ideal: 1080 },
+          height: { ideal: 1920 },
+          aspectRatio: { ideal: 9 / 16 },
+        },
         audio: false,
       })
       if (videoEl) {
