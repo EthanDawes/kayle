97a47b62b7a526c57b07a8ddd30596cc73b62540
Ethan Dawes <ethand.python@gmail.com>
2026-06-27 16:57:50 -0400

    Ratio is still wrong

--- src/lib/UI/Camera.svelte
+++ src/lib/UI/Camera.svelte
@@ -36,7 +36,7 @@
           facingMode: "environment",
           width: { ideal: 1080 },
           height: { ideal: 1920 },
-          aspectRatio: { ideal: 9 / 16 },
+          //aspectRatio: { ideal: 9 / 16 },
         },
         audio: false,
       })
